57d3c7e0dfbe82723480dbb1e0002da52f7fe0ef
Rahul Joshi <tj18022826@gmail.com>
2021-07-30 20:47:00 +0530

    class 3

---
+++ Anil/second.py
@@ -0,0 +1,11 @@
+c1="arav"
+_c2="pant"
+
+print(c1)
+print(_c2)
+
+arav="dfhjvbsdbvsduvbds"
+Arav="asncmascmsanc"
+
+print(arav)
+print(Arav)
---
+++ Anil/temp.py
@@ -0,0 +1,2 @@
+print("Today is class 2")
+print("Revision")
